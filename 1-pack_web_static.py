c8a33069560c1735c914c927610a1333d4c55ecb
Obony <faithnyabz69@gmail.com>
2023-07-05 07:33:26 -0700

    Version Update

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,25 +1,64 @@
 #!/usr/bin/python3
 """
-This is a python program that uses Fabric to Tranfer
-file to my server, by first compressing it.
+This python program creates archive on local machine,
+deploy it to the target servers and unzip it with some
+other cool stuffs
 """
 
-from fabric.api import local
-from time import strftime
-from datetime import time
+
+from fabric.api import *
+from os import path
+from datetime import datetime
 
 
-def do_pack():
+env.hosts = ['34.224.95.198', '52.87.220.62']
+env.rsa = '~/.ssh/id_rsa'
+env.user = 'ubuntu'
+
+
+def do_deploy(archive_path):
+    """Depoying web static
+        :param archive_path: path to archive file
+
     """
-    do_pack funtion or method will create a folder called
-    version and create web_static achive in it with date
-    """
-    filedate = strftime("%Y%m%d%H%M%S")
     try:
-        local("mkdir -p versions")
-        local("tar -czvf versions/web_static_{}.tgz web_static".format(
-            filedate))
-        return "versions/web_static_{}.tgz".format(filedate)
+        if not (path.exists(archive_path)):
+            return False
+
+        # uploading archive
+        put(archive_path, '/tmp/')
+
+        # Creating destination dir
+        time = archive_path[-18:-4]
+        run('sudo mkdir -p /data/web_static/releases/web_static_{}.tgz/'
+            .format(time))
+
+        # Uncompressing archive folder
+        run('tar -xzf /tmp/web_static_{}.tgz -C /data/web_static/\
+            releases/web_static_{}/'.format(time, time))
+
+        # Delete the archive from the web server
+        run('sudo rm /tmp/web_static_{}.tgz'.format(time))
+
+        # Move files
+        run('sudo mv /data/web_static/releases/web_static_{}/web_static/* \
+            /data/web_static/releases/web_static_{}/'.format(time, time))
+
+        # Remove an empty dir
+        run('sudo rm -rf /data/web_static/releases/web_static_{}/\
+            web_static'.format(time))
+
+        # Delete the symbolic link /data/web_static/current from the web server
+        run('sudo rm -rf /data/web_static/current')
+
+        # Create a new the symbolic link /data/web_static/current on
+        # the web server
+        run('sudo ln -sfn /data/web_static/releases/web_static_{}.tgz/ \
+            /data/web_static/current'.format(time))
 
     except Exception as e:
-        return None
+        # If failed
+        return False
+
+        # On Success
+    return True
