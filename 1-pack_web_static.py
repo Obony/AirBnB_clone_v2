e9993772f8e1cfa0f8a3dfaaeb3adbca95105daa
Obony <faithnyabz69@gmail.com>
2023-07-07 13:51:56 -0700

    Deploy archive!

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,24 +1,26 @@
 #!/usr/bin/python3
 """A module for Fabric script that generates a .tgz archive."""
 import os
-from datetime import datetime
+from time import strftime
 from fabric.api import local, runs_once
 
 
 @runs_once
 def do_pack():
     """Archives the static files."""
+    time = strftime("%Y%m%d%H%M%S")
     if not os.path.isdir("versions"):
         os.mkdir("versions")
-    d_time = datetime.now()
-    output = "versions/web_static_{}{}{}{}{}{}.tgz".format(
-        d_time.year,
-        d_time.month,
-        d_time.day,
-        d_time.hour,
-        d_time.minute,
-        d_time.second
-    )
+    # d_time = datetime.now()
+    output = "versions/web_static_{}.tgz".format(time)
+    # output = "versions/web_static_{}{}{}{}{}{}.tgz".format(
+    # d_time.year,
+    # d_time.month,
+    # d_time.day,
+    # d_time.hour,
+    # d_time.minute,
+    # d_time.second
+    # )
     try:
         print("Packing web_static to {}".format(output))
         local("tar -cvzf {} web_static".format(output))
